dab94bfa7289252a422e52679b844467a10747b8
沫烬染 <mojinran@outlook.com>
2024-01-20 02:51:15 +0800

    更新 1.0.0 版本，适配 LiteLoaderQQNT 1.0.0

--- README.md
+++ README.md
@@ -1,27 +1,18 @@
 # chii-devtools
 
-LiteLoaderQQNT插件，使用Chii的DevTools进行远程调试  
+LiteLoaderQQNT 插件，使用 Chii 进行远程调试  
+使用上跟普通的 DevTools 差不多，但缺失了一些功能
 
 
 ## 安装方法
 
-一般情况下无需安装，LiteLoaderQQNT会调用QQNT的DevTools  
-但目前还未找到Windows QQNT 9.8.5版本打开自身DevTools方法  
-使用上跟普通的DevTools差不多，但缺失了一些的功能，只是能用的状态
+### 通过 Release
 
-### 通过插件市场（首选）
+下载最新的 `Release` 的 `chii-devtools.zip`，解压到 `chii-devtools` 放到 `插件目录`
 
-- 如题，你可以在插件市场下载到此插件，来源是最新的Release版本
+### 通过 Clone
 
-### 通过Release（推荐）
-
-- 下载最新的`Release`的`chii-devtools.zip`，解压出`chii-devtools`放到`插件目录`
-- 重启QQNT，按F12会在新窗口打开DevTools
-
-### 通过Clone（不推荐）
-
-安装之前请先确保已安装Node.js，能够使用npm工具
+安装之前请先确保已安装 `Node.js`，能够使用 `npm` 工具
 
 - 克隆/下载本项目，并将其放到插件目录下
-- 进入本插件的目录内，执行命令`npm install chii --save`
-- 重启QQNT，按F12会在新窗口打开DevTools
+- 进入本插件的目录内，执行命令 `npm install chii --save`
--- src/renderer.js
+++ src/renderer.js
@@ -1,23 +1,15 @@
 function injectChiiDevtools(port) {
-    const before_script = document.querySelector("#chii_devtools");
-    if (before_script) {
-        before_script.remove();
-    }
-    const url = `http://localhost:${port}/target.js`;
     const script = document.createElement("script");
     script.defer = "defer";
-    script.src = url;
-    script.id = "chii_devtools";
-    document.head.appendChild(script);
+    script.src = `http://localhost:${port}/target.js`;
+    document.head.append(script);
 }
 
 
-export function onLoad() {
-    chii_devtools.ready(port => {
-        // 监听页面变化
+chii_devtools.ready.then(port => {
+    // 监听页面变化
+    injectChiiDevtools(port);
+    navigation.addEventListener("navigatesuccess", () => {
         injectChiiDevtools(port);
-        navigation.addEventListener("navigatesuccess", () => {
-            injectChiiDevtools(port);
-        });
     });
-}
+});
